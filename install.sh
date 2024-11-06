046872f5403cf9376453cbe8281a8248d570a1b8
Ciaran Moore <ciaran.f.moore@gmail.com>
2024-11-06 11:50:58 +0000

    Amended Install script

--- install.sh
+++ install.sh
@@ -22,35 +22,29 @@ else
   echo "Cloning CPP Repo"
   git clone --recursive https://github.com/datastax/cpp-driver $BASE_DIR/lib/cpp-driver >/dev/null 2>&1
   echo "Compiling cpp-driver..."
-  cd $BASE_DIR/lib/cpp-driver
-  mkdir build && cd build
-  cmake -DCASS_CPP_STANDARD=17 \
+  mkdir $BASE_DIR/lib/cpp-driver/build && cd $BASE_DIR/lib/cpp-driver/build
+  cd $BASE_DIR/lib/cpp-driver/build && cmake -DCASS_CPP_STANDARD=17 \
         -DCASS_BUILD_STATIC=ON \
         -DCASS_BUILD_SHARED=ON \
         -DCASS_USE_TIMERFD=ON \
         -DCASS_USE_LIBSSH2=ON -DCASS_USE_ZLIB=ON \
         -DCMAKE_C_FLAGS="-fPIC" \
-        -DCMAKE_CXX_FLAGS="-fPIC \
-        -Wno-error=redundant-move" \
+        -DCMAKE_CXX_FLAGS="-fPIC" \
+        -Wno-error="redundant-move" \
         -DCMAKE_BUILD_TYPE="RelWithInfo" \
         -DCMAKE_INSTALL_PREFIX=/usr/local \
         -DCMAKE_INSTALL_LIBDIR=lib \
         -DCASS_INSTALL_PKG_CONFIG=ON ..
-  make
-  sudo make install
+  cd $BASE_DIR/lib/cpp-driver/build && make
+  cd $BASE_DIR/lib/cpp-driver/build && sudo make install
   cd $BASE_DIR
-  sudo rm -Rf lib/cpp-driver
+  sudo rm -Rf cd $BASE_DIR/lib/cpp-driver
 fi
 
 
 echo "Compiling and installing the extension..."
-cd ext
+cd $BASE_DIR/ext && phpize
 
-phpize
-
-echo ./configure --with-cassandra=/usr/local --with-libdir=lib
-LIBS="-lssl -lz -luv -lm -lstdc++" LDFLAGS="-L/usr/local/lib" ./configure --with-cassandra=/usr/local --with-libdir=lib
-make
-make install
-
-rm -Rf /tmp/php-driver-installation
+cd $BASE_DIR/ext && ./configure
+cd $BASE_DIR/ext && make
+cd $BASE_DIR/ext && sudo make install
